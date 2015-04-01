213ab9a9b31456db45c00dd010a8282d4f43b5c6
Sylvain Gautier <sylvain@imenlo.com>
2015-04-01 11:22:54 +0200

    Specify seconds for Time creation
    Otherwise the current system seconds is used

--- lib/reform/form/multi_parameter_attributes.rb
+++ lib/reform/form/multi_parameter_attributes.rb
@@ -36,7 +36,7 @@ Reform::Form.class_eval do
         if hour.blank? && minute.blank?
           Date.new(year.to_i, month.to_i, day.to_i) # TODO: test fails.
         else
-          args = [year, month, day, hour, minute].map(&:to_i)
+          args = [year, month, day, hour, minute, 0].map(&:to_i)
           Time.zone ? Time.zone.local(*args) :
             Time.new(*args)
         end
